a456960ebd7f7b876b15112f4bc787ddda18f004
kachkacha2003 <lasha.katchkatchishvili.1@btu.edu.ge>
2023-10-06 21:19:38 +0400

    commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// 1) დავწეროთ ფუნქცია,რომელიც დაპრინტავს "hello world";
+
+// function greeting(){
+//     return "hello world";
+// };
+
+// let resuilt=greeting();
+// console.log(resuilt);
+
+// function world(){
+//     console.log("bitcamp");
+// }
+// world();
+
+// 2)დავწეროთ ფუნქცია,რომელიც გამოიტანს ორი რიცხვის ჯამს.
+
+// function sum(x,y,z,h,l,n){
+//     return h+n;
+//     // console.log(z+h);
+// }
+// let resuilt=sum(5,3,1,5,4,6);
+// console.log(resuilt);
+// // sum(7,7);
+// // sum(8,4,5,6)
+
+// function sum(x,y){
+//   return x+y;
+// }
+// let resuilt=sum(5,4);
+
+// let secondNumber=6;
+// console.log(resuilt+secondNumber);
+
+// 3 დავწეროთ ფუნქცია,რომელიც არგუმენტად იღებს ტექსტს და აბრუნებს ტექსტის სიგრძეს.
+// let letter="bitcamp";
+// console.log(findLength());
+// function findLength(text){
+    
+   
+//     return letter
+// }
+
+
+
+
+
+// 4 დავწეროთ ფუნქცია,რომელსაც არგუმენტად ექნება 2 რიცხვი და დააბრუნებს ამ ორი რიცხვის გაყოფის ნაშთს.
+
+
+// function operation(num1,num2){
+//     return num1%num2;
+// }
+// console.log(operation(6,4))
+
